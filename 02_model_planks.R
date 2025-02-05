b6edc1fb762817249388f39deb5dfa2d6d3376b3
James Robinson <jpwrobinson@gmail.com>
2025-02-05 17:23:26 +0000

    trying model structure

--- 02_model_planks.R
+++ 02_model_planks.R
@@ -6,53 +6,44 @@ depth_ime<-depth %>%
   mutate(island = ISLAND) %>% 
   left_join(ime)
  
-
 # From Richardson sup mat 
-# form <- ~ DEPTH_c +
-#   POP_STATUS +
-#   SITE_SLOPE_400m_c +
-#   DEPTH_c:POP_STATUS +
-#   #s(DEPTH_c, by=POP_STATUS) +
-#   s(DEPTH_c, SITE_SLOPE_400m_c) +
-#   (1|OBS_YEAR) +
-#   (1|OBS_YEAR:ECOREGION) + 
-#   (1|OBS_YEAR:ISLAND) + 
-#   (1|ECOREGION)  + 
-#   (1|SITE) +
-#   (1|DIVER) +
-#   (1+DEPTH_c||ISLAND)
+fix<- ~ DEPTH_c +
+  POP_STATUS +
+  SITE_SLOPE_400m_c +
+  # DEPTH_c:POP_STATUS +
+  #s(DEPTH_c, by=POP_STATUS) +
+  # s(DEPTH_c, SITE_SLOPE_400m_c)
+  (1|OBS_YEAR) +
+  # (1|OBS_YEAR:ECOREGION) +
+  # (1|OBS_YEAR:ISLAND) +
+  (1|ISLAND) +
+  # (1|ECOREGION)  +
+  (1|SITE) 
+  # (1|DIVER)
+  # (1+DEPTH_c||ISLAND)
+
+fix2 <- ~DEPTH_c +
+  POP_STATUS +
+  SITE_SLOPE_400m_c +
+  mean_ime_percent + max_chl + months_ime +
+  (1|OBS_YEAR) +
+  (1|ISLAND) +
+  (1|SITE) 
+
+# with Richardson covariates
+pos_form <- formula(paste(c('PLANKTIVORE', fix), collapse = " ")) # Formula for models with positive data only
+bin_form <- formula(paste(c('hu', fix), collapse = " "))
+
+# with IME covariates
+pos_form2 <- formula(paste(c('PLANKTIVORE', fix2), collapse = " ")) # Formula for models with positive data only
+bin_form2 <- formula(paste(c('hu', fix2), collapse = " "))
 
 
-
-m1<-brm(PLANKTIVORE ~ DEPTH_c +
-    POP_STATUS +
-    SITE_SLOPE_400m_c +
-    DEPTH_c:POP_STATUS +
-    s(DEPTH_c, SITE_SLOPE_400m_c) +
-    (1|OBS_YEAR) +
-    (1|OBS_YEAR:ECOREGION) +
-    (1|OBS_YEAR:ISLAND) +
-    (1|ECOREGION)  +
-    (1|SITE) +
-    (1|DIVER) +
-    (1+DEPTH_c||ISLAND), 
-    family = hurdle_gamma, data = depth_ime,
+m1<-brm(bf(pos_form, bin_form), family = hurdle_gamma, data = depth_ime,
     chains = 3, iter = 2000, warmup = 500, cores = 4)
 
-m1<-brm(PLANKTIVORE ~ DEPTH_c +
-          POP_STATUS +
-          SITE_SLOPE_400m_c +
-          DEPTH_c:POP_STATUS +
-          s(DEPTH_c, SITE_SLOPE_400m_c) +
-          max_chl + mean_ime_percent + months_ime +
-          (1|OBS_YEAR) +
-          (1|OBS_YEAR:ECOREGION) +
-          (1|OBS_YEAR:ISLAND) +
-          (1|ECOREGION)  +
-          (1|SITE) +
-          (1|DIVER) +
-          (1+DEPTH_c||ISLAND), 
-        family = hurdle_gamma, data = depth_ime,
+m1_ime<-brm(bf(pos_form2, bin_form2), family = hurdle_gamma, data = depth_ime,
         chains = 3, iter = 2000, warmup = 500, cores = 4)
 
-
+summary(m1_ime)
+loo(m1, m1_ime)
