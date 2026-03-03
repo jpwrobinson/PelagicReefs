b46ce7369f8fe80ff13ff4d4a5dfc5002b61c150
James Robinson <jpwrobinson@gmail.com>
2026-03-03 16:04:40 +0000

    fish mod update
    Former-commit-id: 6335c21d4624cdd8265303635dc0855b66038fc6

--- Figure2.R
+++ Figure2.R
@@ -16,26 +16,26 @@ effects <- rbind(
   m2_herb %>%
     gather_draws(`b_.*`, regex=TRUE) %>%  
     mutate(fg = 'Herbivore')) %>% 
+  filter(.variable != 'Intercept') %>% 
   mutate(.variable = str_replace_all(.variable, 'b_', ''),
          var_fac = factor(.variable, 
-                          levels = rev(c('Intercept', 'geomorphic_typeIsland','reef_area_km2','island_area_km2',
-                                         'avg_monthly_mm', #'population_statusU',
-                                         'mld_amp',
-                                         'site_bathy_400m', 'hard_coral', 'depth_m'
+                          levels = rev(c('geomorphic_typeIsland','reef_area_km2','island_area_km2','site_bathy_400m',
+                                         'avg_monthly_mm','mld_amp', 
+                                         'hard_coral', 'depth_m'
                                          )))) %>% 
   filter(!is.na(var_fac)) %>% 
-  group_by(var_fac) %>% mutate(medi = abs(median(.value)))
+  group_by(var_fac) %>% mutate(medi = abs(median(.value))) 
 
 
 # Plot effect sizes
-labs<-data.frame(x = Inf, y = c(3.4, 5.4, 8.4), label = c('Site', 'Oceanographic', 'Geomorphic'), fg='Herbivore')
+labs<-data.frame(x = Inf, y = c(2.4, 4.4, 8.4), label = c('Habitat', 'Oceanographic', 'Geomorphic'), fg='Herbivore')
 
-pdf(file = 'fig/Figure2.pdf', height=5, width=7)
+pdf(file = 'fig/Figure2.pdf', height=5, width=9)
 print(
-  ggplot(effects %>% filter(!var_fac=='Intercept'), aes(x = .value, y = var_fac, col = fg)) +
-    geom_text(data = labs, aes(x, y, label = label), size=4, fontface=1, hjust=1, col='black') +
-    annotate('rect', xmin = -Inf, xmax=Inf, ymin = -Inf, ymax = 3.5, fill='grey', alpha=0.1) +
-    annotate('rect', xmin = -Inf, xmax=Inf, ymin = 5.5, ymax = Inf, fill='grey', alpha=0.1) +
+  ggplot(effects, aes(x = .value, y = var_fac, col = fg)) +
+    geom_text(data = labs, aes(x, y, label = label), size=3.5, fontface=1, hjust=1, col='black') +
+    annotate('rect', xmin = -Inf, xmax=Inf, ymin = -Inf, ymax = 2.5, fill='grey', alpha=0.1) +
+    annotate('rect', xmin = -Inf, xmax=Inf, ymin = 4.5, ymax = Inf, fill='grey', alpha=0.1) +
     geom_vline(xintercept = 0, linetype = "dashed", color = "black") + 
     stat_pointinterval(.width = c(0.5, 0.95), pch=19, 
                        position = position_dodge(width=0.65)) +  
@@ -45,10 +45,13 @@ print(
     labs(x = "Effect on metabolic flux", y = "") +
     scale_x_continuous(limits=c(-.9, 0.7), expand=c(0,0)) +
     guides(color='none') +
-    scale_y_discrete(labels = c('Depth', 'Hard coral','Bathymetric slope',
-                                'Mixed layer depth', 'Precipitation', 'Island area', 'Reef area', 'Geomorphic [island]')) +
-    theme(strip.text = element_text(face=2, hjust=0, size=12),
-          strip.background = element_blank())
+    scale_y_discrete(labels = c('Depth', 'Hard coral',
+                                'Mixed layer depth', 'Precipitation', 
+                                'Bathymetric slope','Island area', 'Reef area', 'Geomorphic [island]'), sec.axis = dup_axis()) +
+    theme(strip.text = element_text(face=2, hjust=0, size=11),
+          strip.background = element_blank(),
+          axis.text.y = element_text(size =10),
+          axis.text.x = element_text(size =10))
 )
 dev.off()
 
