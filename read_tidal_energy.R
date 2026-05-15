61720fc9837f52a3903e3ca3f5cecbf41125471e
James Robinson <jpwrobinson@gmail.com>
2026-05-15 11:53:06 +0100

    matching tidal to crep

--- read_tidal_energy.R
+++ read_tidal_energy.R
@@ -114,10 +114,12 @@ for(i in 1:length(file.list)){
   tidal_matrix <- tidal_matrix[nrow(tidal_matrix):1, ]
   
   
+  lon180 <- ifelse(lon > 180, lon - 360, lon)
+  
   # Create raster
   # terra expects matrix in [row, col] = [lat, lon] order
   tidal_rast <- rast(tidal_matrix, 
-                     extent = ext(min(lon), max(lon), min(lat), max(lat)),
+                     extent = ext(min(lon180), max(lon180), min(lat), max(lat)),
                      crs = "EPSG:4326")
   
   # plot to pdf
@@ -129,4 +131,29 @@ for(i in 1:length(file.list)){
 
 dev.off()
 
-save(Hawaii_1, Hawaii_2, Kiribati, Palmyra, Mariana, file = 'data/tidal/tidal_DAT/tidal_rasters.Rdata')
+tidal_pacific <- merge(Hawaii_1, Hawaii_2, Kiribati, Palmyra, Mariana)
+
+save(tidal_pacific, Hawaii_1, Hawaii_2, Kiribati, Palmyra, Mariana, file = 'data/tidal/tidal_DAT/tidal_rasters.Rdata')
+
+
+crep<-read.csv('data/noaa-crep/crep_full_merged.csv') %>% 
+  distinct(ISLAND, lat, lon, SITEVISITID)
+
+# extract tidal from raster using bilinear interpolation (4 nearest cells)
+crep<-crep %>% mutate(tidal_value = extract(tidal_pacific, cbind(lon, lat), method = 'bilinear')[, 1])
+
+## this is missing entire islands not in the rasters, and some sites that may fall on land or NA cells
+nana<-crep %>% group_by(ISLAND) %>% 
+  summarise(na = length(which(is.na(tidal_value))), n = length(SITEVISITID), prop_na = na/n) %>% data.frame
+
+nana %>% filter(prop_na == 1)
+
+# Check a specific NA site
+problem_site <- crep %>% filter(ISLAND=='Hawaii')
+
+# Plot raster with that site
+plot(Hawaii_1)
+points(problem_site$lon, problem_site$lat, col = "red", pch = 19, cex = 0.5)
+
+# Check cell value directly
+cellFromXY(Hawaii_2, cbind(problem_site$lon, problem_site$lat))
