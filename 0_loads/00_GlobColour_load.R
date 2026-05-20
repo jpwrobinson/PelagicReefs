cd3ae305fa4c7ccb724723cb0e34cfc75ad8f60d
James Robinson <jpwrobinson@gmail.com>
2026-05-20 07:18:19 +0100

    globcolour ime strenght

--- 0_loads/00_GlobColour_load.R
+++ 0_loads/00_GlobColour_load.R
@@ -108,6 +108,7 @@ ime_df <- bind_rows(df_list) %>%
 
 write.csv(ime_df, file = 'data/GlobColour/GlobColour_IME_output.csv', row.names=FALSE)
 
+ime_df<-read.csv('data/GlobColour/GlobColour_IME_output.csv')
 island.vec<-ime_df %>% distinct(island) %>% pull(island)
 
 pdf(file = 'fig/ime_db/ime_globcol_timeseries.pdf', height=7, width=12)
@@ -142,6 +143,15 @@ ime_df %>% filter(is.na(Chl_increase_nearby)) # 5,020 month~island combos [highe
 ime_df %>% filter(has_IME ==1 & is_primaryIME == 0) # 1,120 month~island combos
 
 1120 / dim(ime_df)[1] * 100 # 10% have an IME from a different island
+
+# how are Chl_increase and IME strength correlated?
+for(i in 1:length(island.vec)){
+with(ime_df %>% filter(!is.na(Chl_increase_nearby) & island %in% island.vec[i]), 
+     print(paste(island.vec[i], round(cor(Chl_increase_nearby, strength_IME), 2)))) # r = 0.86
+}
+
+ggplot(ime_df, aes(Chl_increase_nearby, strength_IME)) + geom_point() +
+  facet_wrap(~island)
 
 label_df <- ime_df %>%
   group_by(island) %>%
