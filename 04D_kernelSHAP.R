15aca4d1c7954ebe0306d149ba7a951a9ea8e30d
James Robinson <jpwrobinson@gmail.com>
2026-06-11 13:55:26 +0100

    fig with shap

--- 04D_kernelSHAP.R
+++ 04D_kernelSHAP.R
@@ -14,42 +14,48 @@ brms_pred_fun <- function(model, newdata) {
 }
 
 # ── covariate groups ──────────────────────────────────────────────────────────
-groups <- list(
+ime_groups <- list(
+  Geomorphic    = c("land_area_km2", "reef_area_km2", "bathymetric_slope", "geomorphic_type"),
+  Oceanographic = c("mld_mean",'mld_anom', "avg_monthly_mm_anom", "ted_mean", 'mean_chlorophyll')
+)
+
+fish_groups <- list(
   Geomorphic    = c("island_area_km2", "reef_area_km2", "site_bathy_400m", "geomorphic_type"),
   Oceanographic = c("mld_mean", "avg_monthly_mm", "ted_mean"),
   Fish_habitat  = c("hard_coral", "depth_m"),
   Human         = c("population_status")
 )
 
-all_features <- unlist(groups, use.names = FALSE)
-
 # ── helper: run kernelshap + aggregate to group R2 ───────────────────────────
-group_shap_r2 <- function(fit, data, groups, bg_n = 100, response_var) {
+group_shap_r2 <- function(fit, data, groups, bg_n = 100, n_explain = 300, response_var) {
   
   all_features <- unlist(groups, use.names=FALSE)
   
   bg <- data |> slice_sample(n = min(bg_n, nrow(data)))
   X  <- data |> select(all_of(all_features)) %>% 
-    slice_sample(n = 300)    # speed up
-    
+    slice_sample(n = min(n_explain, nrow(data))) # for faster runs
   
   ks <- kernelshap(
     object        = fit,
     X             = X,
     bg_X          = bg |> select(all_of(all_features)),
     pred_fun      = brms_pred_fun,
-    feature_names = all_features,
-    parallel = TRUE
+    feature_names = all_features
   )
   
   shap_mat <- as.data.frame(ks$S)
   
   # ── total variance in posterior mean predictions ──────────────────────────
-  pred_means <- brms_pred_fun(fit, data)
+  pred_means <- brms_pred_fun(fit, X)
   pred_var   <- var(pred_means)
   
+  shap_mat |>
+    summarise(across(everything(), var)) |>
+    pivot_longer(everything(), names_to = "feature", values_to = "shap_var") |>
+    arrange(desc(shap_var))
+  
   # ── group-level importance ────────────────────────────────────────────────
-  map_dfr(names(groups), function(grp) {
+  group_importance<-map_dfr(names(groups), function(grp) {
     vars <- groups[[grp]]
     
     if (length(vars) == 1) {
@@ -58,41 +64,118 @@ group_shap_r2 <- function(fit, data, groups, bg_n = 100, response_var) {
       obs_imp <- rowSums(abs(shap_mat[, vars]))
     }
     
+    # average magnitude of effect across observations.
     mean_abs_shap <- mean(obs_imp)
     
+    # total SHAP variance for abs_R2
+    # heterogeneity of effect across observations
+    total_shap_var <- sum(sapply(names(shap_mat), function(v) var(shap_mat[[v]])))
+    
+    # careful with abs_R2: positive covariance in groups (e.g. big islands = big reefs) can inflate shap values
     tibble(
       group         = grp,
       response      = response_var,
       mean_abs_shap = mean_abs_shap,
       rel_R2        = NA_real_,   # filled below after summing all groups
-      abs_R2 = var(rowSums(shap_mat[, vars, drop = FALSE])) / pred_var
+      abs_R2 = sum(sapply(vars, function(v) var(shap_mat[[v]]))) / total_shap_var
     )
   }) |>
     mutate(rel_R2 = mean_abs_shap / sum(mean_abs_shap))
+  
+  # ── variable-level importance ────────────────────────────────────────────────
+  var_importance <- shap_mat |>
+    summarise(across(everything(), ~mean(abs(.)))) |>
+    pivot_longer(everything(), 
+                 names_to  = "feature", 
+                 values_to = "mean_abs_shap") |>
+    mutate(response = response_var) |>
+    arrange(desc(mean_abs_shap))
+  
+  return(list(group_importance, var_importance))
 }
 
+# ── run for IME ─────────────────────────────────────────
+load(file = 'results/mod_ime.rds')
+vp_ime <- group_shap_r2(m_chl_inc, mod_dat, 
+                          ime_groups, n_explain = dim(mod_dat)[1], bg_n = 100,
+                          response_var = "IME strength")
+
 # ── run for planktivore and herbivore ─────────────────────────────────────────
-vp_plank <- group_shap_r2(m2_plank, plank_scaled, groups, 
+load('results/mod_planktivore_metabolic.rds')
+vp_plank <- group_shap_r2(m2_plank, plank_scaled, 
+                          fish_groups, n_explain = 300, bg_n = 100,
                           response_var = "Planktivore")
 
-# vp_herb  <- group_shap_r2(m2_herb,  herb_scaled,  groups, 
-#                           response_var = "Herbivore")
+load('results/mod_herbivore_metabolic.rds')
+vp_herb  <- group_shap_r2(m2_herb,  herb_scaled,
+                          fish_groups, n_explain = 300, bg_n = 100,
+                          response_var = "Herbivore")
 
-vp_all <- bind_rows(vp_plank, vp_herb)
+
+vp_all <- bind_rows(vp_plank[[1]], vp_herb[[1]])
+vp_by_var <- bind_rows(vp_plank[[2]], vp_herb[[2]]) %>% 
+  mutate(group = case_when(
+    feature %in% fish_groups$Geomorphic    ~ "Geomorphic",
+    feature %in% fish_groups$Oceanographic ~ "Oceanographic",
+    feature %in% fish_groups$Fish_habitat  ~ "Fish_habitat",
+    feature %in% fish_groups$Human         ~ "Human"))
 
 print(vp_all)
 
+write.csv(vp_all, file='results/fish_SHAP_all.csv', row.names = FALSE)
+write.csv(vp_by_var, file='results/fish_SHAP_byvar.csv', row.names = FALSE)
+
 # ── plot ──────────────────────────────────────────────────────────────────────
+
+# rel_R2 = within-model partioning that ignores unexplained variance
 vp_all |>
   mutate(group = fct_reorder(group, rel_R2)) |>
   ggplot(aes(x = rel_R2, y = group, fill = response)) +
   geom_col(position = position_dodge(width = 0.7), width = 0.6) +
-  scale_x_continuous(labels = scales::percent_format(accuracy = 1)) +
+  scale_x_continuous(expand=c(0,0), labels = scales::percent_format(accuracy = 1)) +
   scale_fill_manual(values = c("Planktivore" = "#2196F3",
                                "Herbivore"   = "#4CAF50")) +
-  labs(x     = "Relative importance (proportion of total SHAP)",
-       y     = NULL,
-       fill  = NULL,
-       title = "Variance partitioning via Kernel SHAP") +
-  theme_minimal(base_size = 12) +
-  theme(legend.position = "top")
+  labs(x     = "Variable importance (proportion of total SHAP)",
+       y     = NULL, fill  = NULL) 
+
+vp_all |>
+  mutate(group = fct_reorder(group, rel_R2)) |>
+  ggplot(aes(x = rel_R2, y = response, fill = group)) +
+  geom_col(position='stack') +
+  scale_x_continuous(expand=c(0,0), labels = scales::percent_format(accuracy = 1)) +
+  # scale_fill_manual(values = c("Planktivore" = "#2196F3",
+  #                              "Herbivore"   = "#4CAF50")) +
+  labs(x     = "Variable importance (proportion of total SHAP)",
+       y     = NULL, fill  = NULL) 
+
+# mean_SHAP = cross-model comparisons
+vp_by_var |>
+  # mutate(group = fct_reorder(group, mean_abs_shap)) |>
+  ggplot(aes(x = mean_abs_shap, y = group, fill = response)) +
+  geom_bar(stat = 'identity', position = 'stack') +
+  scale_x_continuous(expand=c(0,0.01)) +
+  scale_y_discrete(limits=c('Geomorphic', 'Oceanographic', 'Fish_habitat', 'Human')) +
+  # scale_fill_manual(values = c("Planktivore" = "#2196F3",
+  #                              "Herbivore"   = "#4CAF50")) +
+  labs(x     = "Mean SHAP values",
+       y     = NULL, fill  = NULL)+
+  facet_wrap(~response, nrow=1) +
+  coord_flip() +
+  theme(legend.position='none')
+
+vp_by_var |>
+  group_by(response) %>% 
+  mutate(feature = fct_reorder(feature, mean_abs_shap)) |>
+  ggplot(aes(x = mean_abs_shap, y = feature, fill = response)) +
+  geom_col() +
+  geom_text(aes(label = feature), hjust = -0.05, size=3) +
+  scale_x_continuous(expand=c(0,0)) +
+  # scale_y_discrete(limits=c('Geomorphic', 'Oceanographic', 'Fish_habitat', 'Human')) +
+  scale_fill_manual(values = c("Planktivore" = "#2196F3",
+                               "Herbivore"   = "#4CAF50")) +
+  labs(x     = "Mean SHAP values",
+       y     = NULL, fill  = NULL)+
+  facet_wrap(~response, nrow=1) +
+  # coord_flip() +
+  theme(legend.position='none',
+        axis.text.y = element_blank(), axis.ticks.y = element_blank())
--- Figure4.R
+++ Figure4.R
@@ -5,10 +5,10 @@ load('results/mod_herbivore_metabolic.rds')
 
 bayes<-data.frame(b = c(bayes_R2(m2_plank, re.form=NA)[1,'Estimate'],
                         bayes_R2(m2_herb, re.form=NA)[1,'Estimate']),
-                  x = 0.9, y = c(6.5, 6.5), fg = c('Planktivore', 'Herbivore'))
+                  x = 0.9, y = 11, fg = c('Planktivore', 'Herbivore'))
 
 direc<-data.frame(b = c('Wetter', 'Deeper'),
-                  x = c(0.3, -0.5), 
+                  x = c(0.5, -0.6), 
                   y = c(3.3, 4.3), fg = c('Herbivore', 'Planktivore'))
 
 ## Effect sizes
@@ -40,39 +40,82 @@ gA<-ggplot(effects, aes(x = .value, y = var_fac, col = fg)) +
     # annotate('rect', xmin = -Inf, xmax=Inf, ymin = -Inf, ymax = 2.5, fill='grey', alpha=0.1) +
     # annotate('rect', xmin = -Inf, xmax=Inf, ymin = 4.5, ymax = 8.5, fill='grey', alpha=0.1) +
     geom_vline(xintercept = 0, linetype = "dashed", color = "black") + 
-    stat_pointinterval(data = effects, .width = c(0.5, 0.95), pch=19, 
+    stat_pointinterval(data = effects, .width = c(0.5, 0.95), pch=19, alpha=0.5,
                        position = position_dodge(width=0.65)) +  
-    stat_slabinterval(data = effects %>% filter(excludes_zero == TRUE), .width = c(0.5, 0.95), pch=19, 
+    stat_pointinterval(data = effects %>% filter(excludes_zero == TRUE), 
+                      .width = c(0.5, 0.95), pch=19, 
                      position = position_dodge(width=0.65)) +  
-    geom_text(data = bayes, aes(x = x, y = y, label = paste0('R² = ', round(b*100,1),'% ')), size=2.5) +
+    geom_text(data = bayes, aes(x = x, y = y, label = paste0('R² = ', round(b*100,1),'% ')), size=3.5) +
     geom_text(data = direc, aes(x = x, y = y, label = b), size=2.5) +
     facet_grid(~fct_rev(fg)) +
     scale_color_manual(values = fg_cols) +
     labs(x = "Effect on fish-assemblage respiration rate", y = "") +
     scale_x_continuous(limits=c(-.9, 1.3), expand=c(0,0)) +
     guides(color='none') +
+    coord_cartesian(clip='off') +
     scale_y_discrete(labels = c('Depth', 'Hard coral',
                                 'Precipitation', 'Mixed layer depth','Tidal energy',
                                 'Bathymetric slope','Island area', 'Reef area', 'Island [vs. atoll]', 'Unpopulated'), 
                      sec.axis = dup_axis(labels=NULL)) +
-    theme(strip.text = element_text(face=2, hjust=0, size=11),
+    theme(strip.text = element_text(face=1, hjust=0, vjust=4, size=10),
           strip.background = element_blank(),
           axis.text.y = element_text(size =10),
           axis.text.x = element_text(size =10))
 
+## SHAP panels
 
-# source('fig3C_fish_var_exp.R')
+th<-theme(legend.position='none',
+          axis.text.y = element_blank(), axis.ticks.y = element_blank(),
+          strip.text = element_text(face=2, hjust=0, size=11),
+          strip.background = element_blank(),
+          axis.text.x = element_text(size=10),
+          plot.margin = unit(c(0.1, 1.5, 0, 1), 'cm'))
 
-labs<-data.frame(x = Inf, y = c(2.4, 4.4, 8.4), 
-                 label = c('Habitat', 'Oceanographic', 'Geomorphic'), 
-                 fg='Herbivore')
+labs<-data.frame(label = c('Depth', 'Hard coral',
+                 'Precipitation', 'Mixed layer depth','Tidal energy',
+                 'Bathymetric slope','Island area', 'Reef area', 'Island [vs. atoll]', 'Unpopulated'),
+                 feature = c('depth_m','hard_coral',
+                   'avg_monthly_mm', 'mld_mean','ted_mean',
+                   'site_bathy_400m','island_area_km2','reef_area_km2', 'geomorphic_type',
+                   'population_status'))
+
+sigs<-effects %>% mutate(response = fg, 
+                         feature = str_replace_all(.variable, 'Island', ''),
+                         feature = str_replace_all(feature, 'statusU', 'status')) %>% 
+  distinct(response, feature, excludes_zero)
+
+vp_by_var<-read.csv('results/fish_SHAP_byvar.csv') %>% 
+  left_join(labs) %>% 
+  left_join(sigs)
 
 
-pdf(file = 'fig/Figure4.pdf', height=4, width=7.5)
+gB1<-ggplot(vp_by_var %>% filter(response=='Planktivore') %>% 
+              mutate(feature = fct_reorder(feature, mean_abs_shap)),
+                                 aes(x = mean_abs_shap, y = feature, alpha=excludes_zero)) +
+  geom_col(fill=fg_cols[2]) +
+  geom_text(aes(label = label), hjust = -0.05, size=3) +
+  scale_x_continuous(expand=c(0,0), limits=c(0, 0.65)) +
+  scale_alpha_manual(values = c(0.5,1)) +
+  coord_cartesian(clip='off') +
+  labs(x = "", y = NULL, fill  = NULL) + th
+
+gB2<-ggplot(vp_by_var %>% filter(response=='Herbivore') %>% 
+              mutate(feature = fct_reorder(feature, mean_abs_shap)),
+            aes(x = mean_abs_shap, y = feature, alpha=excludes_zero)) +
+  geom_col(fill=fg_cols[1]) +
+  geom_text(aes(label = label), hjust = -0.05, size=3) +
+  scale_x_continuous(expand=c(0,0), limits=c(0, 0.65)) +
+  scale_alpha_manual(values = c(0.5,1)) +
+  coord_cartesian(clip='off') +
+  labs(x  = "Variable importance (mean SHAP)", y = NULL, fill  = NULL) + th
+
+gB<-plot_grid(gB1, gB2, nrow=2)
+
+pdf(file = 'fig/Figure4.pdf', height=4.5, width=10)
 print(
-  plot_grid(gA,
-            gB + theme(plot.margin = unit(c(.9,.19, .19, -.5), 'cm')),
-            nrow=1, labels=c('a', 'b'), align='hv', rel_widths=c(1, 0.5))
+  plot_grid(gA + theme(plot.margin=unit(c(1.15, 0, 1.25, 0), 'cm')),
+            gB,
+            nrow=1, labels=c('a', 'b'), align='hv', rel_widths=c(1, 1))
 )
 dev.off()
 
